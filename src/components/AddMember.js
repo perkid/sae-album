59831aa4ea41df0c9f337b5bfa63845f1c9440c9
perkid <perkid90@gmail.com>
2019-10-23 14:42:46 +0900

    AddMember

---
+++ src/components/AddMember.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddMember.css';
+
+const AddMember = ({}) => {
+    return (
+        <div className='AddMember'>
+            
+        </div>
+    );
+};
+
+export default AddMember;
